35cf66c2ebb8d525c22cda65ce20299b5d642269
Dani Lyn <dlyn1110@gmail.com>
2019-06-30 19:41:03 +0000

    Automatically backed up by Learn

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -8,20 +8,22 @@ class RestaurantsContainer extends Component {
   render() {
     return (
       <div>
-        <RestaurantInput addRestaurant={this.props.addRestaurant} addReview={this.props.addReview} />
-        <Restaurants restaurants={this.props.restaurants} deleteRestaurants={this.props.deleteRestaurants} />
+        <RestaurantInput addRestaurant={this.props.addRestaurant}/>
+        <Restaurants restaurants={this.props.restaurants} deleteRestaurant={this.props.deleteRestaurant} />
       </div>
     )
   }
 }
 
-const mapStateToProps = store => ({
-  restaurants: store.restaurants
-})
+const mapStateToProps = ({restaurants}) => {
+  return { restaurants }
+}
 
-const mapDispatchToProps = dispatch => ({
-  addRestaurant: text => dispatch({ type: "ADD_RESTAURANT", text}),
-  deleteRestaurant: id => dispatch({ type: "DELETE_RESTAURANT", id})
-})
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: text => dispatch({type: 'ADD_RESTAURANT', text }),
+    deleteRestaurant: id => dispatch({type: 'DELETE_RESTAURANT', id })
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer)
